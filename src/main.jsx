0880d6294c73b53ba36ef34249090f50f3ab6064
Aminul islam <aminurrohman98@gmail.com>
2026-04-02 22:56:18 +0100

    install React-Toastify

--- src/main.jsx
+++ src/main.jsx
@@ -14,5 +14,6 @@ const router = createBrowserRouter([
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router}></RouterProvider>
+    <ToastContainer />
   </StrictMode>,
 );
