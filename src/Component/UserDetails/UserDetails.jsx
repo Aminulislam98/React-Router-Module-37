703c824ea631f4aa3760802ee7b39959d787c8e3
Aminul islam <aminurrohman98@gmail.com>
2026-04-03 19:48:06 +0100

    navigate step back by using useNavigate()

--- src/Component/UserDetails/UserDetails.jsx
+++ src/Component/UserDetails/UserDetails.jsx
@@ -3,18 +3,17 @@ import { useLoaderData, useNavigate } from "react-router";
 
 const UserDetails = () => {
   const userData = useLoaderData();
-  console.log(userData);
-  const navigate = useNavigate();
-  const handleNavigate = () => {
-    navigate("/Users");
-  };
+  const backNavigate = useNavigate();
+
   return (
-    <div className="flex text-green-800 flex-col justify-center items-center p-10 rounded-2xl border-2 border-green-300 bg-green-100 m-20">
+    <div className="flex gap-y-4  text-green-800 flex-col justify-center items-center p-10 rounded-2xl border-2 border-green-300 bg-green-100 m-20">
       <p className="font-semibold text-green-700">User Name</p>
       {userData.name}
-      <button onClick={handleNavigate} className="btn btn-primary mt-4">
-        Go to User Home section
-      </button>
+      <div className="flex justify-start items-center w-full">
+        <button className="btn btn-success " onClick={() => backNavigate(-1)}>
+          Go Back
+        </button>
+      </div>
     </div>
   );
 };
