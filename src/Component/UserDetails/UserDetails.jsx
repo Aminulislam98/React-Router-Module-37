55bb19deec3a4e8ad7b53f57b9e74ba73153e6bd
Aminul islam <aminurrohman98@gmail.com>
2026-04-03 19:00:08 +0100

    refactor user component

--- src/Component/UserDetails/UserDetails.jsx
+++ src/Component/UserDetails/UserDetails.jsx
@@ -4,7 +4,12 @@ import { useLoaderData } from "react-router";
 const UserDetails = () => {
   const userData = useLoaderData();
   console.log(userData);
-  return <div>{userData.name}</div>;
+  return (
+    <div className="flex text-green-800 flex-col justify-center items-center p-10 rounded-2xl border-2 border-green-300 bg-green-100 m-20">
+      <p className="font-semibold text-green-700">User Name</p>
+      {userData.name}
+    </div>
+  );
 };
 
 export default UserDetails;
