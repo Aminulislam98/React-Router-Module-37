254fc58ccefc1410cfc71f12c1b2cbe53d962506
Aminul islam <aminurrohman98@gmail.com>
2026-04-03 12:35:54 +0100

    made side bar to show the data by path

--- src/Component/Root/Root.jsx
+++ src/Component/Root/Root.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import { Outlet } from "react-router";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import Sidebar from "../Sidebar/Sidebar";
 
 const Root = () => {
   return (
     <div>
       <Header></Header>
-      <Outlet></Outlet>
+      <div>
+        <aside>
+          <Sidebar></Sidebar>
+        </aside>
+        <Outlet></Outlet>
+      </div>
       <Footer></Footer>
     </div>
   );
---
+++ src/Component/Sidebar/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <aside>
+      <nav>
+        <p>Nav bar 1</p>
+        <p>Nav bar 2</p>
+        <p>Nav bar 3</p>
+        <p>Nav bar 4</p>
+        <p>Nav bar 5</p>
+      </nav>
+    </aside>
+  );
+};
+
+export default Sidebar;
